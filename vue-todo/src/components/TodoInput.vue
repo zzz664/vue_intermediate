a1fd212ab57b6576cd2b8898274bf1b4daa7dc3f
zzz664 <zzz664>
2021-12-05 19:28:44 +0900

    vuex 주요 기술 적용

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -32,7 +32,7 @@ export default {
         addTodo() {
             if(this.newTodoItem !== "")
             {
-                this.$emit('addTodoItem', this.newTodoItem);
+                this.$store.commit('addOneItem', this.newTodoItem);
                 this.clearInput();
             } else {
                 this.showModal = !this.showModal;
